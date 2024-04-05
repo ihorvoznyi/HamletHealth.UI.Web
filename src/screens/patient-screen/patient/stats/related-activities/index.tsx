5bcf353107111c329094ca402fd5017b2d191bb5
Andre Nimchuk <andriinim@teampro.com>
2024-04-05 18:12:59 +0300

    feat: check pipes

--- src/screens/patient-screen/patient/stats/related-activities/index.tsx
+++ src/screens/patient-screen/patient/stats/related-activities/index.tsx
@@ -1,88 +1,99 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useState } from "react";
 
-import { MoodType } from '@shared/lib/types';
+import { MoodType } from "@shared/lib/types";
 
-import MoodSvg from './mood-svg';
-import Activity, { IActivity } from './activity';
-import { CoffeeSvg, FootballSvg, FriendsSvg, GiftSvg, MovieSvg, ShoppingSvg } from '@components/ui/svg';
+import MoodSvg from "./mood-svg";
+import Activity, { IActivity } from "./activity";
+import {
+  CoffeeSvg,
+  FootballSvg,
+  FriendsSvg,
+  GiftSvg,
+  MovieSvg,
+  ShoppingSvg,
+} from "@components/ui/svg";
 
-import { classes } from './index.tailwind';
+import { classes } from "./index.tailwind";
 
 const RelatedActivities = () => {
-  const [activeMood, setActiveMood] = useState<MoodType>('great');
+  const [activeMood, setActiveMood] = useState<MoodType>("great");
 
   const renderMoodSvg = (mood: MoodType) => {
     const isActive = activeMood === mood;
-    const color = isActive ? '#12C28D' : 'black';
-
+    const color = isActive ? "#12C28D" : "black";
+    console.log(color);
     return (
-      <MoodSvg 
+      <MoodSvg
         mood={mood}
         width={45}
         height={45}
         color={color}
         styles={{
-          text: isActive 
+          text: isActive
             ? `text-[${color}] border-b-2 border-[${color}]`
-            : `text-[${color}]`
+            : `text-[${color}]`,
         }}
         onClick={() => setActiveMood(mood)}
       />
     );
   };
-  
+
   return (
     <div className={classes.container}>
       <h1 className={classes.title}>Related Activities</h1>
 
       <div className={classes.moods}>
-        {moods.map(mood => <Fragment key={mood}>{renderMoodSvg(mood)}</Fragment>)}
+        {moods.map((mood) => (
+          <Fragment key={mood}>{renderMoodSvg(mood)}</Fragment>
+        ))}
       </div>
 
       <ul className={classes.activities}>
-        {activities.map(activity => <Activity key={activity.text} {...activity} />)}
+        {activities.map((activity) => (
+          <Activity key={activity.text} {...activity} />
+        ))}
       </ul>
     </div>
   );
 };
 
-const moods: MoodType[] = ['great', 'good', 'meh', 'bad', 'awful'];
+const moods: MoodType[] = ["great", "good", "meh", "bad", "awful"];
 const activities: IActivity[] = [
   {
     Icon: CoffeeSvg,
-    text: 'Date',
+    text: "Date",
     times: 12,
-    percentage: 24
+    percentage: 24,
   },
   {
     Icon: FriendsSvg,
-    text: 'Friends',
+    text: "Friends",
     times: 44,
-    percentage: 55
+    percentage: 55,
   },
   {
     Icon: MovieSvg,
-    text: 'Movie',
+    text: "Movie",
     times: 8,
-    percentage: 15
+    percentage: 15,
   },
   {
     Icon: ShoppingSvg,
-    text: 'Shopping',
+    text: "Shopping",
     times: 28,
-    percentage: 45
+    percentage: 45,
   },
   {
     Icon: GiftSvg,
-    text: 'Volunteer',
+    text: "Volunteer",
     times: 20,
-    percentage: 18
+    percentage: 18,
   },
   {
     Icon: FootballSvg,
-    text: 'Football',
+    text: "Football",
     times: 33,
-    percentage: 44
+    percentage: 44,
   },
 ];
 
