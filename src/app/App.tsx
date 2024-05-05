b92ec06eeaaf758fa179370f71503049f42213d1
Ihor Voznyi <ihor.voznyi@tech-stack.io>
2024-05-05 23:59:19 +0300

    chore: remove unnecessary providers component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,20 @@
-import AppProvider from './providers';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-const App = () => {
-  return <AppProvider />;
-};
+import Auth from '@pages/index';
+import { GlobalLoader } from '@components/ui/common';
+
+import { store } from './store';
+
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <GlobalLoader />
+      <Auth />
+      <ToastContainer />
+    </BrowserRouter>
+  </Provider>
+);
 
 export default App;
